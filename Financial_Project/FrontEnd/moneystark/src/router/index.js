7f3d994df3c9a98dd633605d9add1bf33f9900fa
임재열 <a01092201761@gmail.com>
2024-11-18 15:26:54 +0900

    241118 front

--- Financial_Project/FrontEnd/moneystark/src/router/index.js
+++ Financial_Project/FrontEnd/moneystark/src/router/index.js
@@ -2,6 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LogInView from "@/views/LogInView.vue";
 import SignUpView from "@/views/SignUpView.vue";
+import ChartView from "@/components/ChartView.vue";
+import ComparisonView from "@/components/ComparisonView.vue";
+import ProductSuggestionView from "@/components/ProductSuggestionView.vue";
+import CurrencyCalculatorView from "@/components/CurrencyCalculatorView.vue";
+import BankView from "@/components/BankView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +27,31 @@ const router = createRouter({
       name: "SignUpView",
       component: SignUpView,
     },
+    {
+      path: "/chart",
+      name: "ChartView",
+      component: ChartView,
+    },
+    {
+      path: "/comparison",
+      name: "ComparisonView",
+      component:ComparisonView
+    },
+    {
+      path: "/suggestion",
+      name: "ProductSuggestionView",
+      component: ProductSuggestionView
+    },
+    {
+      path: "/currency",
+      name: "CurrencyCalculatorView",
+      component:CurrencyCalculatorView
+    },
+    {
+      path: "/bank",
+      name: "BankView",
+      component: BankView
+    }
   ],
 });
 
